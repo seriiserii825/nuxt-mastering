64cef10245c313331f54f74029784d95bfce0fa1
serii burduja <seriiburduja@gmail.com>
2025-01-10 18:38:00 +0200

    feat: link to first lesson

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { chapters } = useCourse()
+const first_lesson = chapters[0].lessons[0]
+</script>
 
 <template>
   <div class="index">
     <h3 class="text-xl font-bold text-center">index page</h3>
+    <nuxt-link v-if="first_lesson" :to="first_lesson.path">Go to first lesson</nuxt-link>
   </div>
 </template>
