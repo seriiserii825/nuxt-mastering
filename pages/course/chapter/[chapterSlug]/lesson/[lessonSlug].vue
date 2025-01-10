eeb05992fb9cb8932f5e31d6662dc33c2fd1410f
serii burduja <seriiburduja@gmail.com>
2025-01-10 11:20:46 +0200

    feat: nested routes

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="">
+    <h2>Lesson</h2>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
